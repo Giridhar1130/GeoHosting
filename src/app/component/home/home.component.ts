1fad47ed9baac390b7dec006103455eae99ec84d
Sreekanth Jagaleti (Zen3 Infosolutions America Inc) <v-srjaga@microsoft.com>
2019-06-29 12:57:18 -0700

    HomePageThirdTab

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CountryGeoClearanceService } from '../country-geo-clearance/country-geo-clearance.service';
+import { CountryList } from '../types/country.type';
+
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private countryGeoClearanceService: CountryGeoClearanceService) { }
+  public CountriesList: CountryList[] = [];
 
   ngOnInit() {
+    this.countryGeoClearanceService.getAllCountriesWithRiskDetails()
+      .subscribe((data: CountryList[]) => {
+        this.CountriesList = data;
+    });
   }
 
 }
